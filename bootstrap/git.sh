fc0e6553898c4ed2e7a7943ff8ca992c45958d64
Shitikov Kirill <sh.kiruh@gmail.com>
2018-06-20 06:40:03 +0300

    Egite git script

--- bootstrap/git.sh
+++ bootstrap/git.sh
@@ -5,3 +5,10 @@ cd $PROJECT_DIR
 echo -e "\e[34mGIT"
 git submodule init
 git submodule update
+
+
+cd $PROJECT_DIR/RentalEmber
+git checkout master
+
+cd $PROJECT_DIR/RentalDjango
+git checkout master
