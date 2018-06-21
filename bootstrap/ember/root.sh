d6ccb85450b9b17a817effb1665cd8c31abe0d5f
Shitikov Kirill <sh.kiruh@gmail.com>
2018-06-21 22:49:47 +0300

    Add watchman watchman

--- bootstrap/ember/root.sh
+++ bootstrap/ember/root.sh
@@ -6,3 +6,6 @@ pacman -S nodejs npm --noconfirm
 echo -e "\e[34mGlobal npm dependency"
 npm install -g ember-cli
 npm install -g bower
+
+echo -e "\e[34mInstall watchman"
+yaourt -S watchman --noconfirm
